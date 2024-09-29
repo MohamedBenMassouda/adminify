2b107568d3386fcb69c671fe32b01c6e01860580
Mohamed Ben Massouda <benmassoudamohamed1@gmail.com>
2024-09-29 20:48:25 +0100

    Interface to interact with module

---
+++ pkg/admin/panel.go
@@ -0,0 +1,48 @@
+package admin
+
+import (
+	"database/sql"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/MohamedBenMassouda/adminify/internal/database"
+	"github.com/MohamedBenMassouda/adminify/internal/handler"
+	"github.com/MohamedBenMassouda/adminify/internal/model"
+)
+
+type Panel struct {
+	models    map[string]*model.Model
+	db        *database.DB
+	templates *template.Template
+}
+
+func NewPanel(db *sql.DB) (*Panel, error) {
+	templates, err := template.ParseGlob("templates/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Panel{
+		models:    make(map[string]*model.Model),
+		db:        database.NewDB(db),
+		templates: templates,
+	}, nil
+}
+
+func (p *Panel) RegisterModel(modelStruct interface{}, tableName string) error {
+	model, err := model.New(modelStruct, tableName)
+	if err != nil {
+		return err
+	}
+	p.models[model.TableName] = model
+	return nil
+}
+
+func (p *Panel) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	switch r.URL.Path {
+	case "/list":
+		handler.List(w, r, p.db, p.models, p.templates)
+	}
+}
