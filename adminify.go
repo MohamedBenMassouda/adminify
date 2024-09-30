b94e872d09a1af395b51d768ec1cc089fb942ce5
Mohamed Ben Massouda <benmassoudamohamed1@gmail.com>
2024-09-30 16:07:35 +0100

    Added support to gin

--- adminify.go
+++ adminify.go
@@ -6,14 +6,15 @@ import (
 	"net/http"
 
 	"github.com/MohamedBenMassouda/adminify/pkg/admin"
+	"github.com/gin-gonic/gin"
 )
 
 type Adminify struct {
 	Panel *admin.Panel
 }
 
-func NewAdminify(db *sql.DB) Adminify {
-	panel, err := admin.NewPanel(db)
+func NewAdminify(db *sql.DB, path string) Adminify {
+	panel, err := admin.NewPanel(db, path)
 
 	if err != nil {
 		log.Fatal(err)
@@ -34,4 +35,10 @@ func (a *Adminify) RegisterModel(modelStruct interface{}, tableName string) {
 
 func (a *Adminify) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.Panel.ServeHTTP(w, r)
+}
+
+// RegisterGinRoute registers the admin panel to a Gin route
+func (a *Adminify) RegisterGinRoute(r *gin.Engine) {
+	r.StaticFS(a.Panel.Path+"/static", a.Panel.GetStaticFS()) // Serve the static files
+	r.Any(a.Panel.Path+"/*any", gin.WrapH(a.Panel))           // Wrap the Adminify panel in a Gin handler
 }
